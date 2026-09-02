8d7d74845026382effd5590ae2d53698a0d8adbd
Dilshan9210 <dilshan2020dila@gmail.com>
2026-09-02 23:16:42 +0530

    Initial commit

---
+++ src/main/java/com/uovt/student_management_system/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.uovt.student_management_system.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
